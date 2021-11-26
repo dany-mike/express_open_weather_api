675383f60c36d5f70698cca553433bd4715d53c7
Dany-Mike <dmjoseph@kaliop.com>
2021-11-26 14:54:44 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require("cors");
+
+const app = express()
+const port = 3000
+
+app.use(express.json());
+app.use(cors({
+  origin: `*`
+}));
+
+app.get('/', (req, res) => {
+  res.send('Root endpoint')
+})
+
+const citiesRoute = require('./routes/cities.route');
+
+app.use('/cities', citiesRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
