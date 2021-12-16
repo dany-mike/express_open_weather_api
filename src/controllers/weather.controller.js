871000edb416ca10c8a45da003abdf3e6365ba20
Dany-Mike <dmjoseph@kaliop.com>
2021-12-16 15:59:39 +0100

    add forecasts endpoints

--- src/controllers/weather.controller.js
+++ src/controllers/weather.controller.js
@@ -1,10 +1,29 @@
 const openWeather = require('../services/openWeather.service.js')
 
 const cityController = {
-  getWeatherByCityName: async (req, res) => {
+  getCurrentWeatherByCityName: async (req, res) => {
     try {
-      const response = await openWeather.get(`/weather?q=${req.params.city}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+      const response = await openWeather.get(`/weather?q=${req.params.city}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=metric`)
       res.json(response)
+    } catch(err) {
+        res.json({response: err})
+    }
+  },
+  getHourlyForecastByCityName: async (req, res) => {
+    try {
+      const response = await openWeather.get(`/forecast?q=${req.params.city}&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+      res.json(response)
+    } catch(err) {
+        res.json({response: err})
+    }
+  },
+  getDailyForecastByCityNameAndTime: async (req, res) => {
+    try {
+      const response = await openWeather.get(`/forecast?q=${req.params.city}&units=metric&appid=${process.env.OPEN_WEATHER_API_KEY}`)
+      const formattedResponse = response.list.filter((el) => {
+        return el.dt_txt.includes(req.params.time)
+      })
+      res.json(formattedResponse)
     } catch(err) {
         res.json({response: err})
     }
@@ -12,4 +31,3 @@ const cityController = {
 }
 
 module.exports = cityController;
-
--- src/routes/weather.route.js
+++ src/routes/weather.route.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-const { getWeatherByCityName } = require('../controllers/weather.controller.js');
+const { getCurrentWeatherByCityName, getHourlyForecastByCityName, getDailyForecastByCityNameAndTime } = require('../controllers/weather.controller.js');
 
-router.get('/:city', getWeatherByCityName);
+router.get('/current/:city', getCurrentWeatherByCityName);
+
+router.get('/forecast/:city', getHourlyForecastByCityName);
+
+router.get('/forecast/daily/:city/:time', getDailyForecastByCityNameAndTime);
 
 module.exports = router;
