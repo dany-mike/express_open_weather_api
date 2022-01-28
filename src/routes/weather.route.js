edec34bd068103960ed58effc7677dd202d7077a
Dany-Mike <dmjoseph@kaliop.com>
2022-01-28 11:50:29 +0100

    [weather-by-lat-and-long] get current weather by lat and long endpoint created

--- src/routes/weather.route.js
+++ src/routes/weather.route.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { getCurrentWeatherByCityName, getHourlyForecastByCityName, getDailyForecastByCityNameAndTime, getCurrentWeatherByPostalCode, getWeatherForecastByPostalCode } = require('../controllers/weather.controller.js');
+const { getCurrentWeatherByCityName, getHourlyForecastByCityName, getDailyForecastByCityNameAndTime, getCurrentWeatherByPostalCode, getWeatherForecastByPostalCode, getCurrentWeatherByLatAndLon, getWeatherForecastByLatAndLon } = require('../controllers/weather.controller.js');
 
 router.get('/current/:city', getCurrentWeatherByCityName);
 
@@ -8,8 +8,10 @@ router.get('/forecast/:city', getHourlyForecastByCityName);
 
 router.get('/forecast/daily/:city/:time', getDailyForecastByCityNameAndTime);
 
-router.get('/current-zip/:zip', getCurrentWeatherByPostalCode)
+router.get('/current-zip/:zip', getCurrentWeatherByPostalCode);
 
-router.get('/forecast-zip/:zip', getWeatherForecastByPostalCode)
+router.get('/forecast-zip/:zip', getWeatherForecastByPostalCode);
+
+router.get('/current/:lat/:lon', getCurrentWeatherByLatAndLon);
 
 module.exports = router;
